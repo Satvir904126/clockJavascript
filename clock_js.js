1117d4b293ac6ac9988b18364153d0319dbf6a48
Satvir Singh Satvir Singh <ssatvirs@isimtl.local>
2020-09-14 16:02:51 -0400

    analog clock

---
+++ clock_js.js
@@ -0,0 +1,38 @@
+console.log('js')
+//startTime()
+function startTime()
+{
+    let clock = new Date();
+    let sec = clock.getSeconds();
+    let min = clock.getMinutes();
+    let hour= clock.getHours();
+  
+    document.querySelector('.sec').style.transform = 'translateX(-50%) rotate(' + (sec * 6) + 'deg)';
+
+    document.querySelector('.min').style.transform = 'translateX(-50%) rotate(' + (min * 6) + 'deg)';
+
+    document.querySelector('.hour').style.transform = 'translateX(-50%) rotate(' + (hour * 30) + 'deg)';
+  
+  //  document.querySelector('.min').style.transform='translateX(-50%) rotate('+(min * 6)+'deg)';
+    setTimeout(startTime,500) 
+}
+startTime()
+
+// startTime()
+
+// function startTime() {
+//     let clock = new Date();
+//     let sec = clock.getSeconds()
+//     let min = clock.getMinutes()
+//     let hour = clock.getHours()
+//    // document.querySelector('.sec').style.transform = 'translateX(-50%) rotate(' + (sec * 6) + 'deg)';
+//     document.querySelector('.sec').style.transform = '(+translateX(-50%) rotate( + (sec * 6) + deg)'
+
+//     //  document.querySelector('.sec').style.transform = 'translateX(-50%) rotate(' + (sec * 6) + 'deg)';
+ 
+
+//     //  document.querySelector('.min').style.transform = 'translateX(-50%) rotate(' + (min * 6) + 'deg)';
+
+//     //  document.querySelector('.hour').style.transform = 'translateX(-50%) rotate(' + (hour * 30) + 'deg)';
+//     setTimeout(startTime, 500)
+// }
